ca304a521798cfee2ebed148e24f3f815cb77ba2
Kentarou Fukuda <kentarou@jp.ibm.com>
2023-09-06 11:43:03 +0900

    update links #1

--- projectNav.php
+++ projectNav.php
@@ -2,7 +2,7 @@
 
 /*******************************************************************************
  * @file
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2023 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,7 @@
 
 // PHP for Standard Left Menu.
 $Nav->setLinkList(array());
-$Nav->addCustomNav("Information about ACTF", "/projects/project_summary.php?projectid=technology.actf", "", 1);
+$Nav->addCustomNav("Information about ACTF", "/actf/", "", 1);
 
 $Nav->addCustomNav("Developer Resources", '/actf/contributors.php', "", 1);
 $Nav->addCustomNav("Downloads", '/actf/docs/', "", 1);
@@ -23,7 +23,7 @@ $Nav->addCustomNav("Documentation", '/actf/docs/', "", 1);
 $Nav->addCustomNav("Project Wiki", 'https://wiki.eclipse.org/ACTF', "", 1);
 $Nav->addCustomNav("Team", "/actf/team.php", "", 1);
 $Nav->addCustomNav("Mailing Lists", 'https://accounts.eclipse.org/mailing-list/actf-dev', "", 1);
-$Nav->addCustomNav("Bugs", 'https://bugs.eclipse.org/bugs/buglist.cgi?bug_status=__open__&list_id=21343437&product=ACTF', "", 1);
+$Nav->addCustomNav("Issues", 'https://github.com/eclipse-actf/actf-website/issues', "", 1);
 
 
 
